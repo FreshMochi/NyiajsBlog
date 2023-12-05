af69aa910d56e1384730489e7f47b33f9fe01a85
FreshMochi <jukke.dev@gmail.com>
2023-12-05 01:29:40 -0800

    Changed ./lib/mongodc.js to ./lib/mongodb.ts and utilizing from if to switch in post.js

--- pages/blogs/[postSlug].tsx
+++ pages/blogs/[postSlug].tsx
@@ -1,27 +1,65 @@
 // /pages/blogs/[postSlug].tsx
-
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { connectToMongoDB, client } from '../../lib/mongodb';
 
-interface Props {}
-
-const BlogPage: NextPage<Props> = () => {
-  return <div>single page</div>;
-};
-
-export const getStaticPaths: GetStaticPaths = () => {
-  return {
-    paths: [{params: {postSlug: "Creating-my-first-blog" }}],
-    fallback: false, // we will come back to this later and understand this with example
-  }
+interface Post {
+  title: string;
+  content: string;
+  slug: string;
+  // Add other fields if needed
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+interface Props {
+  post: Post;
+}
 
-  return {
-    props: {
-    },
-  };
+const BlogPage: NextPage<Props> = ({ post }) => {
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  );
 };
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  try {
+    // Connect to MongoDB
+    const db = await connectToMongoDB();
+    const collection = db.collection<{ slug: string }>(process.env.COLLECTION);
+
+    // Fetch data here and create paths dynamically
+    const posts = await collection.find({}).toArray();
+    const paths = posts.map((post) => ({ params: { postSlug: post.slug } }));
+
+    return {
+      paths,
+      fallback: false,
+    };
+  } catch (error) {
+    console.error('Error connecting to MongoDB or fetching data:', error);
+    throw new Error('Internal Server Error');
+  }
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  try {
+    // Connect to MongoDB
+    const db = await connectToMongoDB();
+    const collection = db.collection<Post>(process.env.COLLECTION);
+
+    // Fetch specific data for the given postSlug
+    const post = await collection.findOne({ slug: params?.postSlug });
+
+    return {
+      props: {
+        post,
+      },
+    };
+  } catch (error) {
+    console.error('Error connecting to MongoDB or fetching data:', error);
+    throw new Error('Internal Server Error');
+  }
+};
 
 export default BlogPage;
