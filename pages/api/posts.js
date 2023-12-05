865978679e4c91e3206d7a4667fa13ba22c9993d
FreshMochi <jukke.dev@gmail.com>
2023-12-05 01:29:40 -0800

    Changed ./lib/mongodc.js to ./lib/mongodb.ts and utilizing from if to switch in post.js

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,5 +1,4 @@
 // "./pages/api/posts.js"
-
 import { connectToMongoDB, client } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
@@ -7,28 +6,34 @@ export default async function handler(req, res) {
     // Connect to MongoDB
     await connectToMongoDB();
     console.log(); // Log connection status
-    
+
     const collection = client.db(process.env.DB).collection(process.env.COLLECTION);
 
-    if (req.method === 'POST') {
-      const { title, content } = req.body;
+    switch (req.method) {
+      case 'POST':
+        const { title, content } = req.body;
 
-      // Request validation
-      if (!title || !content) {
-        return res.status(400).json({ success: false, error: 'Bad Request', message: 'Title and content are required' });
-      }
+        // Request validation
+        if (!title || !content) {
+          return res.status(400).json({ success: false, error: 'Bad Request', message: 'Title and content are required' });
+        }
 
-      // Insert new post into the collection
-      await collection.insertOne({ title, content });
+        // Insert new post into the collection
+        await collection.insertOne({ title, content });
 
-      // Respond with success message
-      res.status(201).json({ success: true, message: 'Post created successfully' });
-    } else if (req.method === 'GET') {
-      // Handle GET request to retrieve posts
-      const posts = await collection.find({}).toArray();
-      res.status(200).json({ success: true, posts });
-    } else {
-      res.status(405).json({ success: false, error: 'Method Not Allowed' });
+        // Respond with success message
+        res.status(201).json({ success: true, message: 'Post created successfully' });
+        break;
+
+      case 'GET':
+        // Handle GET request to retrieve posts
+        const posts = await collection.find({}).toArray();
+        res.status(200).json({ success: true, posts });
+        break;
+
+      default:
+        res.status(405).json({ success: false, error: 'Method Not Allowed' });
+        break;
     }
   } catch (error) {
     // Error handling with more details
