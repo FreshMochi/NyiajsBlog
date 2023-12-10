e5712ef2a2c197fcee735b6d37444c5f96f2032b
FreshMochi <jukke.dev@gmail.com>
2023-12-10 11:46:54 -0800

    Changed monogoDB credientials, modified slug page to dynamic route. slug page is now completed and working as normal

--- pages/blogs/[postSlug].tsx
+++ pages/blogs/[postSlug].tsx
@@ -1,63 +1,74 @@
 // /pages/blogs/[postSlug].tsx
+
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { connectToMongoDB, client } from '../../lib/mongodb';
 
 interface Post {
   title: string;
   content: string;
   slug: string;
-  // Add other fields if needed
 }
 
-interface Props {
-  post: Post;
+interface Articles {
+  posts: Post[];
 }
 
-const BlogPage: NextPage<Props> = ({ post }) => {
+const BlogPage: NextPage<{ article: Post }> = ({ article }) => {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    <div className='bg-blue-800'>
+      <h1>Title: {article.title}</h1>
+      <p>{article.content}</p>
     </div>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
   try {
-    // Connect to MongoDB
-    const db = await connectToMongoDB();
-    const collection = db.collection<{ slug: string }>(process.env.COLLECTION);
+    await connectToMongoDB();
 
-    // Fetch data here and create paths dynamically
-    const posts = await collection.find({}).toArray();
+    const collection = client.db(process.env.DB).collection(process.env.COLLECTION);
+
+    const posts = await collection.find().toArray();
+
     const paths = posts.map((post) => ({ params: { postSlug: post.slug } }));
 
     return {
       paths,
-      fallback: false,
+      fallback: true,
     };
   } catch (error) {
-    console.error('Error connecting to MongoDB or fetching data:', error);
+    console.error('Error fetching dynamic paths:', error);
     throw new Error('Internal Server Error');
   }
 };
 
-export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
-    // Connect to MongoDB
-    const db = await connectToMongoDB();
-    const collection = db.collection<Post>(process.env.COLLECTION);
+    await connectToMongoDB();
+    const collection = client.db(process.env.DB).collection(process.env.COLLECTION);
 
-    // Fetch specific data for the given postSlug
-    const post = await collection.findOne({ slug: params?.postSlug });
+    const post = await collection.findOne({ slug: params.postSlug });
+
+    if (!post) {
+      console.error('Post not found for slug:', params.postSlug);
+      throw new Error('Post not found');
+    }
+
+    // Convert _id to a string before returning
+    const { _id, ...restOfPost } = post;
+    const article = { ...restOfPost, _id: _id.toString() };
 
     return {
       props: {
-        post,
+        article,
       },
+      revalidate: 10,
     };
   } catch (error) {
-    console.error('Error connecting to MongoDB or fetching data:', error);
+    console.error('Error fetching article:', error);
     throw new Error('Internal Server Error');
   }
 };
