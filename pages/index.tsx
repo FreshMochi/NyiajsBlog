6633f86bee89fc7b1606a45619e7d9c2ef1c670b
Nyiaj <jukke.dev@gmail.com>
2023-12-29 22:02:06 -0800

    Added style module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,16 @@
 import { Inter } from 'next/font/google'
+import style from '../styles/index.module.css'
 import bgImg from '../public//Wallpaper.jpg'
 import Image from "next/image";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const style = {
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-  }
   return (
-    <div className={'w-full max-w-96 relative flex'}>
-      <div className={`blur-sm -z-50`}>
+    <div className={style.container}>
+      <div className={`blur-sm`}>
         <Image
-        sizes='full'
-        style={style}
         src={bgImg} 
-        priority
         alt="Hero Image"/>
       </div>
       <div className='absolute' >
