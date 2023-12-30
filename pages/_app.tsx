8b81ab74dc113e6be3a72f1695d5efe69ead81ad
Nyiaj <jukke.dev@gmail.com>
2023-12-29 20:50:19 -0800

    Added Hero image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,10 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className='overflow-hidden'>
     <Navbar />
     <RouterWrapper>
       <Component {...pageProps} />
-
     </RouterWrapper>
-    </>
+    </div>
 )}
