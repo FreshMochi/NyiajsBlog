587816006ac6e24426279ebd734b73b270442543
FreshMochi <jukke.dev@gmail.com>
2024-01-04 11:39:50 -0800

    added icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import style from '../styles/index.module.css'
 import bgImg from '../public//Wallpaper.jpg'
+import jukkeIcon from '../public/Jukke-icon.svg'
 import Image from "next/image";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,17 +10,25 @@ export default function Home() {
   return (
     <div className={style.container}>
       <div className={style.heroContainer}>
-        <div className={`blur-sm overflow-x-hidden absolute -z-50`}>
+        <div 
+        className={`
+        blur-sm 
+        overflow-x-hidden 
+        absolute -z-50
+        
+        `}>
           <Image
           style={{overflow: 'hidden'}}
           priority
           src={bgImg} 
           alt="Hero Image"/>
         </div>
-        <div className='' >
-            <h1>
-              Hello
-            </h1>
+        <div className={style.iconSVG} >
+        <Image
+          style={{overflow: 'hidden', height: 'inherit'}}
+          priority
+          src={jukkeIcon} 
+          alt="icon"/>
         </div>
         <div className='' >
           <h1>
