c8c79be9314b0342d30aeec8be02066f242c4137
FreshMochi <jukke.dev@gmail.com>
2024-01-21 19:10:22 -0800

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
-
+// _document.tsx
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link rel="icon" 
+        href="/jukke-icon-blk.ico"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
